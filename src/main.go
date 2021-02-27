be22a10004c7a2ddea04746838f10875bbbfa6cb
이승기 <iseung-gi@iseung-giui-MacBookAir.local>
2021-02-27 21:15:32 +0900

    URLcheckerWithGolang

---
+++ src/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type result struct {
+	url    string
+	status string
+}
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func hitURL(url string, c chan result) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "Failed"
+	}
+	c <- result{url: url, status: status}
+}
